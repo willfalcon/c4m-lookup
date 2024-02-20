e3c48e321e11555ae491fe0e4a3a9c8288666508
Will Hawks <willkhawks@gmail.com>
2024-02-20 16:50:44 -0600

    stat text sizes and animation

--- blocks/lookup/app/Data.js
+++ blocks/lookup/app/Data.js
@@ -1,12 +1,49 @@
 import classNames from 'classnames';
 import { h } from 'preact';
+import { useEffect, useRef, useState } from 'preact/hooks';
+import textFit from 'textfit';
 
-export default function Data({ icon, value, label, bgColor, valueColor, labelColor }) {
+export default function Data({ icon, value, label, bgColor, valueColor, labelColor, delay }) {
+  const ref = useRef(null);
+  const [spanFlex, setSpanFlex] = useState(true);
+  const [spanHeight, setSpanHeight] = useState('auto');
+  const [entered, enter] = useState(false);
+  useEffect(() => {
+    if (ref.current) {
+      const size = ref.current.getBoundingClientRect();
+      ref.current.style.width = `${size.width}px`;
+      eight = `${size.height}px`;
+      setSpanHeight(`${size.height}px`);
+      textFit(ref.current, {
+        maxFontSize: 100,
+      });
+      setSpanHeight('auto');
+      setSpanFlex(false);
+      setTimeout(() => {
+        enter(true);
+      }, delay);
+    }
+  }, [ref.current]);
+
   return (
-    <div class={classNames('rounded-full aspect-square text-center grid-rows-3 justify-items-center', bgColor)}>
+    <div
+      class={classNames(
+        'rounded-full aspect-square text-center flex flex-col items-center content-center pb-6 transition duration-200',
+        bgColor,
+        {
+          '-translate-y-2': entered,
+          'opacity-0': !entered,
+        }
+      )}
+    >
       <img src={`../wp-content/plugins/c4m-lookup/dist/${icon}.png`} class="w-20 h-20 inline-block -mt-9 mx-auto" />
-      <div class="row-span-2 w-full">
-        <span class={classNames('block text-2xl font-bold w-[80%] mx-auto', valueColor)}>{value}</span>
+      <div class="row-span-2 w-full flex-1 flex flex-col">
+        <span
+          class={classNames('font-bold w-[80%] mx-auto *:leading-none', valueColor, { 'flex-1': spanFlex, height: spanHeight })}
+          ref={ref}
+        >
+          {value}
+        </span>
         <span class={classNames('block font-semibold w-[80%] mx-auto text-base', labelColor)}>{label}</span>
       </div>
     </div>
--- blocks/lookup/app/DataCard.js
+++ blocks/lookup/app/DataCard.js
@@ -1,6 +1,8 @@
 import { h } from 'preact';
 import Data from './Data';
 import { X } from 'lucide-preact';
+import { useEffect, useState } from 'preact/hooks';
+import classNames from 'classnames';
 
 export default function DataCard(props) {
   const { searchField, fields, careReduction, jobs, taxRevenue, setOpenCounty } = props;
@@ -23,9 +25,14 @@ export default function DataCard(props) {
   const jobsIcon = jobs.icon_select;
   const taxRevenueIcon = taxRevenue.icon_select;
 
+  const [entered, enter] = useState(false);
+  useEffect(() => {
+    enter(true);
+  }, []);
+
   return (
     // <div class="absolute w-full h-full z-10 top-0 left-0 bg-gradient-to-tr from-c4m-yellow-300 to-c4m-blue-400 to-60%">
-    <div class="absolute w-full h-full z-10 top-0 left-0 card-gradient">
+    <div class="absolute w-full h-full z-10 top-0 left-0 card-gradient flex flex-col justify-evenly">
       <button
         class="close-button absolute top-2 right-2 z-10"
         onClick={() => {
@@ -35,12 +42,15 @@ export default function DataCard(props) {
         <X />
       </button>
       <img
-        class="county-bg absolute w-full h-full object-contain"
+        class={classNames('county-bg absolute w-full h-full object-contain transition duration-300', {
+          'scale-50': !entered,
+          'opacity-0': !entered,
+        })}
         src={`../wp-content/plugins/c4m-lookup/blocks/lookup/counties/${county.toLowerCase()}.svg`}
         alt={`${county} outline`}
       />
       <h3 class="text-center uppercase text-white">
-        <span class="block font-extrabold text-8xl">{county}</span> <span class="block font-bold text-4xl">County</span>
+        <span class="block font-extrabold text-7xl">{county}</span> <span class="block font-bold text-4xl">County</span>
       </h3>
       <div class="grid grid-cols-3 gap-6 px-4 mt-9">
         <Data
@@ -50,8 +60,17 @@ export default function DataCard(props) {
           bgColor="bg-c4m-blue-700"
           valueColor="text-c4m-yellow-200"
           labelColor="text-c4m-blue-100"
+          delay={0}
         />
-        <Data icon={jobsIcon} value={jobsValue} label="Jobs" bgColor="bg-c4m-yellow-400" valueColor="text-white" labelColor="text-white" />
+        <Data
+          icon={jobsIcon}
+          value={jobsValue}
+          label="Jobs"
+          bgColor="bg-c4m-yellow-400"
+          valueColor="text-white"
+          labelColor="text-white"
+          delay={100}
+        />
         <Data
           icon={taxRevenueIcon}
           value={taxRevenueValue}
@@ -59,6 +78,7 @@ export default function DataCard(props) {
           bgColor="bg-c4m-blue-100"
           valueColor="text-c4m-blue-700"
           labelColor="text-c4m-blue-700"
+          delay={200}
         />
       </div>
     </div>
