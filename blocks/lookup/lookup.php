7b228a3ce8890a09a2a02e9905a31b8d53175c83
Will Hawks <willkhawks@gmail.com>
2024-03-01 10:55:08 -0600

    backend bock preview

--- blocks/lookup/lookup.php
+++ blocks/lookup/lookup.php
@@ -14,5 +14,31 @@
   }
 ?>
 
-<div class="<?php echo $classes; ?>">
-</div>
+<?php if ($is_preview) : ?>
+  <div class="<?php echo $classes; ?>" style="float: none; width:600px; margin-left:auto;margin-right:auto;">
+    <div style="border:2px black;border-radius:12px;aspect-ratio:3/2;position:relative;display:flex;flex-direction:column;">
+      <div style="padding:12px;">
+        <h2 style="font-weight:bold;font-size:24px;line-height:32px;text-align:center;color:white;">
+          Look up how your local economy can see a boost by closing the health insurance coverage gap.
+        </h2>
+      </div>
+      <div style="padding:12px;flex: 1 1 0%;">
+        <div style="padding:16px;padding-top:48px;padding:48px 32px 16px;z-index:0;">
+          <label htmlFor="location-search" style="text-align:center;">
+            Start typing your county name:
+          </label>
+          <div>
+            <input
+              name="location-search"
+              id="location-search"
+              style="border-color:black;border-radius:4px;padding:8px 16px;width:100%;"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php else: ?>
+  <div class="<?php echo $classes; ?>">
+  </div>
+<?php endif; ?>
